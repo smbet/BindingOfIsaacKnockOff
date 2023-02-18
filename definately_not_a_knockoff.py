31c332b0851fe863ef6bcabc18bab8c78621e544
Joshua Maldonado <joshuamaldonado4432@gmail.com>
2023-02-18 00:34:47 -0500

    Can shoot either up, down, left, or right. Holding down shoot button works :)

--- definately_not_a_knockoff.py
+++ definately_not_a_knockoff.py
@@ -31,18 +31,16 @@ dt = clock.tick(FPS)
 
 SPEED_CORRECTION = 1 / math.sqrt(2)
 PLAYER_SPEED = 500
-BULLET_SPEED = 5
+BULLET_SPEED = 700
 
+BULLET_COOLDOWN = FPS // 5  # shoots X times per sec in 'FPS // X
+bullet_shot_at  = 0         # tracks when bullet is shot in terms of ticks
+bullet_shotQ    = False     # tracks to see if a bullet was shot (so you can't spam in multiple directions)
+
+# all_of_the_bullets will store each bullet as a list of length 2 in the format:
+#          all_of_the_bullets = [ [bullet_rect_object, "bullet_direction"] ]
+# example: all_of_the_bullets = [ [bullet_rect, "E"], [bullet_rect, "NW"], [bullet_rect, "SE"], [bullet_rect, "S"] ]
 all_of_the_bullets = []
-bullets_N  = []
-bullets_NW = []
-bullets_W  = []
-bullets_SW = []
-bullets_S  = []
-bullets_SE = []
-bullets_E  = []
-bullets_NE = []
-
 
 # define lots of colors
 BLACK   = (0,   0,   0)
@@ -73,19 +71,17 @@ bullet      = pygame.image.load("resources/tictac_20x20.png").convert()
 total_num_of_ticks  = 0
 the_game_is_running = True
 while the_game_is_running:
+    bullet_shotQ = False
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             the_game_is_running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                bullets_E.append(
-                                #   bullet.get_rect(center = player_pos), 
-                                  pygame.Rect(player_pos[0], player_pos[1], 15, 15)
-                                #   [player_pos[0], player_pos[1]]  # assigned like this instead of 'player_pos' to 'untie' variables
-                                  )
-                all_of_the_bullets.append( bullets_E[-1] )
-    
-    
+        # if event.type == pygame.KEYDOWN:
+            # if event.key == pygame.K_UP:
+                # all_of_the_bullets.append([
+                #                            pygame.Rect(player_pos[0], player_pos[1], 15, 15),
+                #                            "N"
+                #                            ])
 
     pressed_keys = pygame.key.get_pressed()
     if pressed_keys[pygame.K_ESCAPE]:
@@ -116,26 +112,72 @@ while the_game_is_running:
             player_pos[0] += SPEED_CORRECTION * PLAYER_SPEED / dt
         else:
             player_pos[0] += PLAYER_SPEED / dt
-        
-    # if pressed_keys[pygame.K_LSHIFT] or pressed_keys[pygame.K_RSHIFT]:
-    #     # shoot()
-    #     bullet      = pygame.image.load("resources/tictac_20x20.png").convert()   # THIS IS SUPPOSED TO GIVE A SURFACE BUT IT'S GIVING ME A RECT :((((((
-    #     bullet_rect = bullet.get_rect(center = player_pos)
-    #     bullet_pos  = player_pos
-    #     bullets.append([bullet_rect, bullet_pos])
+    
 
+    if total_num_of_ticks > (bullet_shot_at + BULLET_COOLDOWN):
+        if pressed_keys[pygame.K_UP] and not bullet_shotQ:
+            all_of_the_bullets.append([
+                                    #    pygame.Rect(player_pos[0], player_pos[1], 15, 15),
+                                    pygame.Rect(
+                                                player_pos[0] + pygame.Surface.get_width(player)  / 2, 
+                                                player_pos[1] + pygame.Surface.get_height(player) / 2, 
+                                                15, 15),
+                                    "N"
+                                    ])
+            bullet_shot_at = total_num_of_ticks
+            bullet_shotQ   = True
+        if pressed_keys[pygame.K_DOWN] and not bullet_shotQ:
+            all_of_the_bullets.append([
+                                    #    pygame.Rect(player_pos[0], player_pos[1], 15, 15),
+                                    pygame.Rect(
+                                                player_pos[0] + pygame.Surface.get_width(player)  / 2, 
+                                                player_pos[1] + pygame.Surface.get_height(player) / 2, 
+                                                15, 15),
+                                    "S"
+                                    ])
+            bullet_shot_at = total_num_of_ticks
+            bullet_shotQ   = True
+        if pressed_keys[pygame.K_RIGHT] and not bullet_shotQ:
+            all_of_the_bullets.append([
+                                    #    pygame.Rect(player_pos[0], player_pos[1], 15, 15),
+                                    pygame.Rect(
+                                                player_pos[0] + pygame.Surface.get_width(player)  / 2, 
+                                                player_pos[1] + pygame.Surface.get_height(player) / 2, 
+                                                15, 15),
+                                    "E"
+                                    ])
+            bullet_shot_at = total_num_of_ticks
+            bullet_shotQ   = True
+        if pressed_keys[pygame.K_LEFT] and not bullet_shotQ:
+            all_of_the_bullets.append([
+                                    #    pygame.Rect(player_pos[0], player_pos[1], 15, 15),
+                                    pygame.Rect(
+                                                player_pos[0] + pygame.Surface.get_width(player)  / 2, 
+                                                player_pos[1] + pygame.Surface.get_height(player) / 2, 
+                                                15, 15),
+                                    "W"
+                                    ])
+            bullet_shot_at = total_num_of_ticks
+            bullet_shotQ   = True
 
 
     screen.fill(BLACK)
     screen.blit(player, player_pos)
 
     for this_bullet in all_of_the_bullets:
-        if (this_bullet.centerx <= 0) or (this_bullet.centerx >= WIDTH) or (this_bullet.centery <= 0) or (this_bullet.centery >= HEIGHT):
-            this_bullet.remove
+        if (this_bullet[0].centerx <= 0) or (this_bullet[0].centerx >= WIDTH) or (this_bullet[0].centery <= 0) or (this_bullet[0].centery >= HEIGHT):
+            all_of_the_bullets.remove(this_bullet)
     
-    for e in range(len(bullets_E)):
-        bullets_E[e] = pygame.Rect.move(bullets_E[e], BULLET_SPEED, 0)
-        pygame.draw.rect(screen, RED, bullets_E[e])
+    for i in range(len(all_of_the_bullets)):
+        if all_of_the_bullets[i][1] == "N":
+            all_of_the_bullets[i][0] = pygame.Rect.move(all_of_the_bullets[i][0], 0, -BULLET_SPEED / dt)
+        if all_of_the_bullets[i][1] == "S":
+            all_of_the_bullets[i][0] = pygame.Rect.move(all_of_the_bullets[i][0], 0, BULLET_SPEED / dt)
+        if all_of_the_bullets[i][1] == "E":
+            all_of_the_bullets[i][0] = pygame.Rect.move(all_of_the_bullets[i][0], BULLET_SPEED / dt, 0)
+        if all_of_the_bullets[i][1] == "W":
+            all_of_the_bullets[i][0] = pygame.Rect.move(all_of_the_bullets[i][0], -BULLET_SPEED / dt, 0)
+        pygame.draw.rect(screen, RED, all_of_the_bullets[i][0])
     
     pygame.display.update()
     if (total_num_of_ticks % 10 == 0):
